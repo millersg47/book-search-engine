53e302bf87d1b63ea730e6244a306f10659a7f87
Sophie Miller <millersg47@gmail.com>
2022-07-25 14:40:14 -0700

    adds login to resolvers, and updates createUser method

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -8,18 +9,38 @@ const resolvers = {
     },
   },
   Mutation: {
-    createUser: async (parent, args) => {
-      const user = await User.create(args);
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
 
       const token = signToken(user);
       res.json({ token, user });
 
       return user;
     },
+
+    login: async (parent, { body }) => {
+      const user = await Profile.findOne({
+        $or: [{ username: body.username }, { email: body.email }],
+      });
+
+      if (!user) {
+        throw new AuthenticationError("No user with this email found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(body.password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
     saveBook: async (parent, { _id, savedBooks }) => {
       const newBook = await User.findOneAndUpdate(
         { _id },
-        { $addToSet: { savedBooks: body } },
+        { $addToSet: { savedBooks: params.bookId } },
         { new: true }
       );
       return newBook;
