50a2b5db3301ed82eeada243c7af9e912429b15f
Sophie Miller <millersg47@gmail.com>
2022-07-26 10:31:55 -0700

    updates app.js file to include apollo with graphql and adds mutations to front-end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,43 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($profileId: ID!, $skill: String!) {
+    saveBook(profileId: $profileId, skill: $skill) {
+      _id
+      name
+      skills
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+     
+    }
+  }
+`;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,16 +18,14 @@ const resolvers = {
       return user;
     },
 
-    login: async (parent, { body }) => {
-      const user = await Profile.findOne({
-        $or: [{ username: body.username }, { email: body.email }],
-      });
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
       if (!user) {
         throw new AuthenticationError("No user with this email found!");
       }
 
-      const correctPw = await user.isCorrectPassword(body.password);
+      const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
         throw new AuthenticationError("Incorrect password!");
@@ -54,6 +52,8 @@ const resolvers = {
           { new: true }
         );
       }
+
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
